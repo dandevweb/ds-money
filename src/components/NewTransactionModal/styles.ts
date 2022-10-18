2ce6503dac597cc68aa21015982e26cd84c0fe71
dandevweb <danilo.pacheco@soitic.com>
2022-10-17 21:08:22 -0300

    Inserting and retrieving data with miragejs

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { transparentize } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -46,4 +47,54 @@ export const Container = styled.form`
             filter: brightness(0.9);
         }
     }
+`;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#015F43',
+    red: '#AA2834',
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+        height: 4rem;
+        border: none;
+        border-radius: 0.25rem;
+
+        background: ${(props) => props.isActive
+        ? transparentize(0.6, colors[props.activeColor])
+        : '#29292e'
+    };
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        transition: filter 0.3s;
+
+        &:hover  {
+            filter: brightness(1.2);
+        }
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+
+        }
 `;
